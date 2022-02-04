f175b02ff7e825dc4802833897408bb1cf84e1f0
junior rocco <norbertorocco@hotmail.com>
2022-02-04 12:11:09 -0300

    primeiro commit

---
+++ Pessoa.cs
@@ -0,0 +1,14 @@
+﻿
+namespace CadastroAPI.Models
+{
+    public class Pessoa
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string CPF { get; set; }
+        public string Email { get; set; }
+        public string Telefone { get; set; }
+        public string Sexo { get; set; }
+        public string DataNascimento { get; set; }
+    }
+}
---
+++ PessoaContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CadastroAPI.Models
+{
+    public class PessoaContext : DbContext
+    {
+        public PessoaContext(DbContextOptions<PessoaContext> options) : base(options) 
+        { 
+            Database.EnsureCreated();
+        }
+
+        //Mapeamento das Tabelas.
+        public DbSet<Pessoa> Pessoas { get; set; }
+
+    }
+}
